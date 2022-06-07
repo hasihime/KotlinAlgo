2705154461334b7eef97475ee591b8f77976acb6
hasihime <osb330@naver.com>
2022-06-07 22:40:09 +0900

    lambda filter test

--- src/main/kotlin/Person.kt
+++ src/main/kotlin/Person.kt
@@ -1,5 +1,3 @@
 data class Person(val name: String, val age: Int) {
-    fun Maxby(kProperty1: KProperty1<Person, Int>) {
 
-    }
 }
--- src/main/kotlin/salute.kt
+++ src/main/kotlin/salute.kt
@@ -1,5 +1,16 @@
 fun salute()=println("Salute!")
 
+fun Person.isAdult()= age>=21
+
 fun main(args: Array<String>) {
     run(::salute)
+
+    val CreatePerson=::Person
+    val p=CreatePerson("Alice",29)
+    println(p)
+
+    val predicate =Person::isAdult
+
+    println(predicate(p))
+
 }
